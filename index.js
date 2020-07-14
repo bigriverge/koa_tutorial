66da4f594d288f310b599b61f39cbbb1fe387a19
wangjiang <wangjiang@dcloud.io>
2020-07-14 18:11:12 +0800

    restful api

--- index.js
+++ index.js
@@ -4,27 +4,42 @@ const bodyParser = require('koa-bodyparser')
 const Router = require('koa-router')
 const router = new Router()
 
-router.get('/', (ctx, next) => {
-    ctx.type = 'html'
-    let html =
-        `
-            <h1>登录</h1>
-            <form method="POST" action="/">
-                <p>用户名</p>
-                <input name="userName" /><br/>
-                <p>密码</p>
-                <input name="password" /><br/>
-                <button type="submit">submit</button>
-            </form>`
-
-    ctx.body = html
+router.post('/users', (ctx, next) => {
+    ctx.body = '新增了一位用户'
 })
 
-router.post('/', (ctx, next) => {
-    let postData = ctx.request.body
-    ctx.body = postData
+router.del('/users/:id', (ctx, next) => {
+    ctx.body = '删除了用户编号为id的用户'
 })
 
+router.put('/users/:id', (ctx, next) => {
+    ctx.body = '修改了用户编号为id的用户'
+})
+
+router.get('/users/id', (ctx, next) => {
+    ctx.body = '编号为id的用户信息'
+})
+
+// router.get('/', (ctx, next) => {
+//     ctx.type = 'html'
+//     let html =
+//         `
+//             <h1>登录</h1>
+//             <form method="POST" action="/">
+//                 <p>用户名</p>
+//                 <input name="userName" /><br/>
+//                 <p>密码</p>
+//                 <input name="password" /><br/>
+//                 <button type="submit">submit</button>
+//             </form>`
+
+//     ctx.body = html
+// })
+
+// router.post('/', (ctx, next) => {
+//     let postData = ctx.request.body
+//     ctx.body = postData
+// })
 
 app.use(bodyParser()).use(router.routes()).use(router.allowedMethods())
 // app.use(async (ctx, next) => {
